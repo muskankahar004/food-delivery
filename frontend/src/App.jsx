8463907b0494786383070cdbb288904601365a76
muskankahar0402 <muskankahar0402@gmail.com>
2025-02-22 16:19:16 +0530

    commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+     
+      <Navbar/>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      Ragistraion page
+    </div>
+  )
+}
+
+export default Register
